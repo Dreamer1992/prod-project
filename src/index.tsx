3ddc4e1f0e4d719426265845233ff748de56a074
Ganiev Davlet <ganiev.davlet@gmail.com>
2023-05-28 22:06:32 +0300

    4. connect react, scss/sass loader

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import { render } from "react-dom";
+
+// project imports
+import Counter from "./components/Counter";
+
+render(
+  <div>
+    <Counter />
+  </div>,
+  document.getElementById("root")
+);
