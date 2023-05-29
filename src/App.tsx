d3b080cdc1f0f62b6c1ed878226adf19d229a45d
Ganiev Davlet <ganiev.davlet@gmail.com>
2023-05-29 23:27:25 +0300

    6. Routing. Code splitting. Lazy. Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-// project imports
-import Counter from "./components/Counter";
+import { Suspense } from "react";
+import { Route, Routes, Link } from "react-router-dom";
 
 // styles
 import "./index.scss";
 
+// pages
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+
 const App = () => {
   return (
     <div className="app">
-      <Counter />
+      <Link to="/">Главная</Link>
+      <Link to="/about">О сайте</Link>
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/about" element={<AboutPageAsync />} />
+          <Route path="/" element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
 import { render } from "react-dom";
 
+import { BrowserRouter } from "react-router-dom";
+
 // project imports
 import App from "./App";
 
-render(<App />, document.getElementById("root"));
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
