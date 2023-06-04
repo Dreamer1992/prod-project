bed53ea20d432ddb5f50e59eea903ef2eae5d975
Ganiev Davlet <ganiev.davlet@gmail.com>
2023-06-04 22:53:55 +0300

    18. Added eslint, husky

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -3,9 +3,6 @@ import { useTranslation } from 'react-i18next'
 const MainPage = () => {
   const { t } = useTranslation('main')
 
-  console.log(t('Главная страница'))
-  console.log(t('Главная страница'))
-
   return <div>{t('Главная страница')}</div>
 }
 
