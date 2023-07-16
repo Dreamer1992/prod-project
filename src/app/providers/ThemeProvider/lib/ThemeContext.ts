dfb0be4aa72f872acb35e30da4e3f933e55963fb
Ganiev Davlet <davlet@routeamgroup.com>
2023-07-16 21:19:10 +0300

    31. Fix theme for Modal

--- src/app/providers/ThemeProvider/lib/ThemeContext.ts
+++ src/app/providers/ThemeProvider/lib/ThemeContext.ts
@@ -1,15 +1,15 @@
 import { createContext } from 'react'
 
 export const enum Theme {
-  LIGHT = 'light',
-  DARK = 'dark',
+  LIGHT = 'app_light_theme',
+  DARK = 'app_dark_theme',
 }
 
-export interface IThemeContextProps {
+export interface IProps {
   theme?: Theme
   setTheme?: (theme: Theme) => void
 }
 
-export const ThemeContext = createContext<IThemeContextProps>({})
+export const ThemeContext = createContext<IProps>({})
 
 export const LOCAL_STORAGE_THEME_KEY = 'theme'
